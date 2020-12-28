adef6aa5cfb275ce1c1198fcfe534971f2330d89
marcocaffarelli <caffarelli-m94@hotmail.it>
2020-12-28 13:22:28 +0100

    Aggiunta una chat attiva dall'apertura del programma
    Aggiunta una chat attiva dall'apertura del programma

--- milestone-5/assets/js/main.js
+++ milestone-5/assets/js/main.js
@@ -160,7 +160,28 @@ let app = new Vue({
             },
         ],
         chatAttiva: [
-
+            {
+                name: 'Michele',
+                avatar: '_1',
+                visible: true,
+                messages: [
+                {
+                date: '10/01/2020 15:30:55',
+                text: 'Hai portato a spasso il cane?',
+                status: 'sent'
+                },
+                {
+                date: '10/01/2020 15:50:00',
+                text: 'Ricordati di dargli da mangiare',
+                status: 'sent'
+                },
+                {
+                date: '10/01/2020 16:15:22',
+                text: 'Tutto fatto!',
+                status: 'received'
+                }
+                ],
+            },
         ],
         nuovoMes: "",
 
@@ -197,6 +218,7 @@ let app = new Vue({
                 //console.log(element.name.includes(this.cercaNome));
             });
         },
+
         //al click elimina il messaggio
         cancella(index){
             for (let i = 0; i < this.chatAttiva.length; i++) {
